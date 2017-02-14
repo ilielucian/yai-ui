86080bca7bca763a4d6bb2721fc2af1e28d5fb28
Lucian Ilie <ilielucian@outlook.com>
2017-02-14 20:47:35 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/app.template.html',
+})
+export class AppComponent {
+  name = 'Lucian';
+
+  currentUser = 'Lucian';
+
+  storyBy = 'Vlad';
+  storyDate = this.formatDate(new Date());
+  storyTitle = 'What a goal!';
+  storyDescription = 'Hey gaiz, luc uat a gol!!';
+  storyLikes = ['Lucian', 'Georgiana'];
+  storyLiked = this.storyLikes.indexOf(this.currentUser) > -1;
+
+  storyCommentUser = 'Lucian';
+  storyComment = 'Que porqueria!!';
+
+
+  // TODO should be a service
+  formatDate(date: Date) {
+    var monthNames = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
+
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+
+    return day + ' ' + monthNames[monthIndex] + ' ' + year;
+  }
+}
